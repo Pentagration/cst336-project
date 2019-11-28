5ed55c05d2447a46f42e417a7b75f10dab12877d
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-28 02:04:41 +0000

    fix href

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function(req, res) {
             conn.end();
             res.render("index", {"candyInfo":result}
             );
-            console.log(result);
+            //console.log(result);
         });
     });
 });
